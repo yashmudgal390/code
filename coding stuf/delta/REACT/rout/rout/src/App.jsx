0974433fb09cd67165d019b2b18a80614052b324
Yash Mudgal <yashmudgal390@gmail.com>
2026-01-13 23:46:35 +0530

    routing

--- coding stuf/delta/REACT/rout/rout/src/App.jsx
+++ coding stuf/delta/REACT/rout/rout/src/App.jsx
@@ -3,6 +3,8 @@ import { NavLink, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/home';
 import { Cart } from './pages/cart';
 import { About } from './pages/about';
+import { Login } from './pages/login';
+import { PrivateRoute } from './component/PrivateRoute';
 
 function App() {
   function getStyle({ isActive }) {
@@ -18,12 +20,18 @@ function App() {
         <NavLink to="/" style={getStyle}>Home</NavLink>
         <NavLink to="/cart" style={getStyle}>Cart</NavLink>
         <NavLink to="/about" style={getStyle}>About</NavLink>
+        <NavLink to="/login" style={getStyle}>Login</NavLink>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
+        <Route path="/cart" element={
+          <PrivateRoute>
+          <Cart />
+          </PrivateRoute>
+        } />
         <Route path="/about" element={<About />} />
+        <Route path='/login' element={<Login/>}/>
       </Routes>
     </>
   );
