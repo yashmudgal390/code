eb798f98713289a1f7c15d3f6dbdde0e609624fb
Yash Mudgal <yashmudgal390@gmail.com>
2026-01-15 17:01:52 +0530

    redux todo

---
+++ coding-stuf/delta/REACT/redux-todo/slices/todoSlice.jsx
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState ={
+    todos:[],
+}
+const todoSlice = createSlice({
+    name:'todo',
+    initialState,
+    reducers:{
+        addTodo:(state,action)=>{
+            state.todos.push(action.payload)
+        },
+        deleteTodo:(state,action)=>{
+            state.todos=state.todos.filter(todo =>todo.id !== action.payload);
+        }
+    }
+})
+export const {addTodo,deleteTodo} = todoSlice.actions;
+export default todoSlice.reducer;
--- coding-stuf/delta/REACT/redux-todo/src/App.jsx
+++ coding-stuf/delta/REACT/redux-todo/src/App.jsx
@@ -1,14 +1,49 @@
 import './App.css'
-
+import { useDispatch, useSelector } from 'react-redux';
+import { addTodo ,deleteTodo } from '../slices/todoSlice';
+import { v4 as uuid } from 'uuid';
+import { useState } from 'react';
 function App() {
 
+  const [inputText, setInputText] = useState('');
+
+  const { todos } = useSelector(state => state.todos);
+  console.log(todos);
+
+  const dispatch = useDispatch();
+
+  const onAddClick = () => {
+    dispatch(addTodo({
+      id: uuid(),
+      todo: inputText
+    }))
+    setInputText('');
+  }
+  const onTodoDelete=(id)=>{
+    dispatch(deleteTodo(id)); 
+  }
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+
+    <div className="App bg-slate-200 w-full h-full">
+      <h1 className='text-violet-800 pt-4'>Todo App</h1>
+
+      <div>
+
+        <input value={inputText} onChange={(e) => setInputText(e.target.value)} placeholder='Enter a todo...' />
+        <button onClick={onAddClick}>Add</button>
+      </div>
+      {
+        todos?.length > 0 && todos.map(todo => (
+          <div>
+            <span>{todo.todo}</span>
+            <button onClick={() => onTodoDelete(todo.id)}>Delete</button>
+          </div>
+        ))
+      }
+    </div>
+
+
   )
 }
 
